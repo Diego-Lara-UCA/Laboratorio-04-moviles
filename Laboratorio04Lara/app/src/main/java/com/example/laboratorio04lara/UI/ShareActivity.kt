92af3add0875150bfdf00bb2335599454d123546
Diego-Lara-UCA <00138921@uca.edu.sv>
2023-04-12 12:37:36 -0600

    Laboratorio 04 moviles

---
+++ Laboratorio04Lara/app/src/main/java/com/example/laboratorio04lara/UI/ShareActivity.kt
@@ -0,0 +1,28 @@
+package com.example.laboratorio04lara.UI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.example.laboratorio04lara.R
+
+class ShareActivity : AppCompatActivity() {
+    private lateinit var nameTextView: TextView
+    private lateinit var emailTextView: TextView
+    private lateinit var telTextView: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_share)
+
+        nameTextView = findViewById(R.id.name_text_view)
+        emailTextView = findViewById(R.id.email_text_view)
+        telTextView = findViewById(R.id.tel_text_view)
+
+        var nameShared = intent.getStringExtra("name").toString()
+        var emailShared = intent.getStringExtra("email").toString()
+        var telShared = intent.getStringExtra("tel").toString()
+
+        nameTextView.text = "Nombre: ${nameShared}"
+        nameTextView.text = "Correo: ${nameShared}"
+        nameTextView.text = "Teléfono: ${nameShared}"
+    }
+}
---
+++ Laboratorio04Lara/app/src/main/java/com/example/laboratorio04lara/test.kt
@@ -0,0 +1,4 @@
+package com.example.laboratorio04lara
+
+class test {
+}
